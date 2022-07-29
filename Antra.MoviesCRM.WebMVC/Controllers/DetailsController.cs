d5358ae3c1d49e84285430cf8720eaedf76ca1e5
Riku Morishima <riku.morishima@gmail.com>
2022-07-29 00:10:12 -0500

    Adding Details View

---
+++ Antra.MoviesCRM.WebMVC/Controllers/DetailsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Antra.MoviesCRM.WebMVC.Controllers
+{
+    public class DetailsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
