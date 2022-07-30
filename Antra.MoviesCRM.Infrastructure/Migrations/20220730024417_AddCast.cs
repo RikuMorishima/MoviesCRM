dcfc42eb981e37238caa887474f25592c8be9015
Riku Morishima <riku.morishima@gmail.com>
2022-07-29 21:46:10 -0500

    WIP: Update database migration

---
+++ Antra.MoviesCRM.Infrastructure/Migrations/20220730024417_AddCast.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Antra.MoviesCRM.Infrastructure.Migrations
+{
+    public partial class AddCast : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_MovieGenre",
+                table: "MovieGenre");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_MovieCast",
+                table: "MovieCast");
+
+            migrationBuilder.RenameTable(
+                name: "MovieGenre",
+                newName: "MovieGenres");
+
+            migrationBuilder.RenameTable(
+                name: "MovieCast",
+                newName: "MovieCasts");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_MovieGenres",
+                table: "MovieGenres",
+                columns: new[] { "MovieId", "GenreId" });
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_MovieCasts",
+                table: "MovieCasts",
+                columns: new[] { "MovieId", "CastId", "Character" });
+
+            migrationBuilder.CreateTable(
+                name: "Casts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(128)", nullable: true),
+                    Gender = table.Column<string>(type: "nvarchar(MAX)", nullable: true),
+                    TmdbUrl = table.Column<string>(type: "nvarchar(MAX)", nullable: true),
+                    ProfilePath = table.Column<string>(type: "nvarchar(2084)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Casts", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Genres",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(64)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Genres", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Trailers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MovieId = table.Column<int>(type: "int", nullable: false),
+                    TrailerUrl = table.Column<string>(type: "nvarchar(2084)", nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(2084)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Trailers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(128)", nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(128)", nullable: true),
+                    DateOfBirth = table.Column<DateTime>(type: "datetime2(7)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(256)", nullable: true),
+                    HashedPassword = table.Column<string>(type: "nvarchar(1024)", nullable: true),
+                    Salt = table.Column<string>(type: "nvarchar(1024)", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(16)", nullable: true),
+                    TwoFactorEnabled = table.Column<bool>(type: "bit", nullable: true),
+                    LockoutEndDate = table.Column<DateTime>(type: "datetime2(7)", nullable: true),
+                    LastLoginDateTime = table.Column<DateTime>(type: "datetime2(7)", nullable: true),
+                    IsLocked = table.Column<bool>(type: "bit", nullable: true),
+                    AccessFailedCount = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Movies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(256)", nullable: false),
+                    Overview = table.Column<string>(type: "nvarchar(MAX)", nullable: true),
+                    Tagline = table.Column<string>(type: "nvarchar(512)", nullable: true),
+                    Budget = table.Column<decimal>(type: "decimal(18,2)", nullable: true),
+                    Revenue = table.Column<decimal>(type: "decimal(18,2)", nullable: true),
+                    ImdbUrl = table.Column<string>(type: "nvarchar(2084)", nullable: true),
+                    TmdbUrl = table.Column<string>(type: "nvarchar(2084)", nullable: true),
+                    PosterUrl = table.Column<string>(type: "nvarchar(2084)", nullable: true),
+                    BackDropUrl = table.Column<string>(type: "nvarchar(2084)", nullable: true),
+                    OriginalLanguage = table.Column<string>(type: "nvarchar(64)", nullable: true),
+                    ReleaseDate = table.Column<DateTime>(type: "datetime2(7)", nullable: true),
+                    RunTime = table.Column<int>(type: "int", nullable: true),
+                    Price = table.Column<decimal>(type: "decimal(5,2)", nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2(7)", nullable: true),
+                    UpdatedDate = table.Column<DateTime>(type: "datetime2(7)", nullable: true),
+                    UpdatedBy = table.Column<string>(type: "nvarchar(MAX)", nullable: true),
+                    CreatedBy = table.Column<string>(type: "nvarchar(MAX)", nullable: true),
+                    CastId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movies", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Movies_Casts_CastId",
+                        column: x => x.CastId,
+                        principalTable: "Casts",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Movies_CastId",
+                table: "Movies",
+                column: "CastId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Genres");
+
+            migrationBuilder.DropTable(
+                name: "Movies");
+
+            migrationBuilder.DropTable(
+                name: "Trailers");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Casts");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_MovieGenres",
+                table: "MovieGenres");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_MovieCasts",
+                table: "MovieCasts");
+
+            migrationBuilder.RenameTable(
+                name: "MovieGenres",
+                newName: "MovieGenre");
+
+            migrationBuilder.RenameTable(
+                name: "MovieCasts",
+                newName: "MovieCast");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_MovieGenre",
+                table: "MovieGenre",
+                columns: new[] { "MovieId", "GenreId" });
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_MovieCast",
+                table: "MovieCast",
+                columns: new[] { "MovieId", "CastId", "Character" });
+        }
+    }
+}
