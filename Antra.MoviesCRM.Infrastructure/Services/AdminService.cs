d82aee923092856a888a0d77040369f4e865da1f
Riku Morishima <riku.morishima@gmail.com>
2022-08-02 18:52:13 -0500

    Implement methods forgotten

--- Antra.MoviesCRM.Infrastructure/Services/AdminService.cs
+++ Antra.MoviesCRM.Infrastructure/Services/AdminService.cs
@@ -1,8 +1,13 @@
 ﻿using Antra.MoviesCRM.Core.Contracts.Services;
+using Antra.MoviesCRM.Core.Models;
 
 namespace Antra.MoviesCRM.Infrastructure.Services
 {
     public class AdminService : IAdminService
     {
+        public Task<IEnumerable<MoviesReportModel>> GetTopPurchases(DateTime? fromDate = null, DateTime? toDate = null, int pageSize = 30, int pageIndex = 1)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
